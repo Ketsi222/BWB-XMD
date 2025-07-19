dac711d9468fdca70885ae615ee1d9d7026bfacc
Ketsi222 <masingamooketsi1@gmail.com>
2025-07-19 10:53:15 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'BWB XMD',
   author: '‎',
   botName: "BWB XMD",
-  botOwner: 'Princetech', // Your name
-  ownerNumber: '255763834140', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'DADDY YEBZA', // Your name
+  ownerNumber: '27696601357', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
